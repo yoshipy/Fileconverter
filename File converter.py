ff41d1049aaf5e81dbf6c3a0674b3349055e98e6
yoshipy <83287130+yoshipy@users.noreply.github.com>
2022-10-25 19:30:09 +0200

    Add files via upload

--- File converter.py
+++ File converter.py
@@ -2,20 +2,22 @@ import pypandoc
 import easygui
 from easygui import *
 import pathlib
+import os
 
 print('please select a file to convert...')
 
 
-inputfile = easygui.fileopenbox()
+inputfile = easygui.fileopenbox(multiple=True)
 
-inputsuffix = pathlib.Path(inputfile).suffix
+inputsuffix = inputfile[0]
+inputsuffix = pathlib.Path(inputsuffix).suffix
 
 print(inputsuffix)
 
 inputsuffix = inputsuffix.replace('.', '')
 
 
-choices = ['pptx', 'revealjs', 'rst', 'rtf', 's5', 'slideous', 'slidy', 'tei', 'texinfo', 'textile', 'xwiki', 'zimwiki', 'json','docx']
+choices = ['pptx', 'html', 'revealjs', 'rst', 'rtf', 's5', 'slideous', 'slidy', 'tei', 'texinfo', 'textile', 'xwiki', 'zimwiki', 'json','docx', 'md']
 
 
 msg = "What would you like to convert the file to?"
@@ -23,10 +25,16 @@ reply = buttonbox(msg, choices=choices)
 
 print(reply)
 
-outputfile = easygui.filesavebox()
+print('please select a folder to save the converted files in...')
+outputfolder = easygui.diropenbox()
 
-output = pypandoc.convert_file(inputfile, format=inputsuffix, to=reply, outputfile=outputfile+'.'+reply, extra_args=['-RTS'])
+print(outputfolder)
+
+
+for element in inputfile:
+    path = os.path.basename(element)
+    output = pypandoc.convert_file(element, format=inputsuffix, to=reply, outputfile=outputfolder+'/'+path+'.'+reply, extra_args=['-RTS'])
 
 
 
-print('your file has been saved as ' + outputfile)
+print('your file has been saved as ')
